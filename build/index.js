fa7d7df6af1e7d82aa10887470247d25bda7e7f2
Fei Wu <feiyuwu@umich.edu>
2023-07-03 23:53:20 -0400

    07/03/23

--- build/index.js
+++ build/index.js
@@ -1,15 +1,28 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default
-    .readFileSync('football.csv', {
-    encoding: 'utf-8',
-})
-    .split('\n')
-    .map((row) => {
-    return row.split(',');
-});
-console.log(matches);
+const CsvFileReader_1 = require("./CsvFileReader");
+//object, but here it is not clear the purpose (why is the following info important)
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+// const MatchResult = {
+//   homeWin: 'H',
+//   awayWin: 'A',
+//   draw: 'D',
+// };
+//enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+//object, but here it is not clear the purpose (why is the following info important)
 
-console.log(matches);
+const reader = new CsvFileReader('football.csv');
+reader.read();
+// const MatchResult = {
+//   homeWin: 'H',
+//   awayWin: 'A',
+//   draw: 'D',
+// };
+
+//enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
